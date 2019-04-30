0e50d3e4ce8613c6e75ad53900079d39e974a095
James Boullion <jboullion83@gmail.com>
2019-04-29 22:13:37 -0500

    Setup the Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,37 @@
 import React from 'react';
 
-// import HeaderCSS from './Header.module.css';
+import HeaderCSS from './Header.module.css';
+
 
 const header = () => {
-	return <header>
-		<h1>header</h1>
-	</header>
+
+	console.log(HeaderCSS);
+
+	return <header id={HeaderCSS.header} className="navbar navbar-expand-lg navbar-dark bg-primary mb-3">
+			<div className="container">
+				<div className={`navbar-brand ${HeaderCSS.navbarBrand}`} href="/">
+					<img src="/img/logo.svg" width="30" height="30" className="d-inline-block align-top" alt="D and D Tools" />
+					D&amp;D Tools
+				</div>
+				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-menu" aria-controls="nav-menu" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+
+				<div className={`collapse navbar-collapse ${HeaderCSS.navbarCollapse}`} id="nav-menu">
+				
+					<ul className="navbar-nav">
+						<li className={`nav-item ${HeaderCSS.navItem}`}>
+							<button className="w-100 btn btn-outline-info create-account" data-toggle="modal" data-target="#create-account-modal">Create Account</button>
+						</li>
+						<li className={`nav-item ${HeaderCSS.navItem}`}>
+							<button className="w-100 btn btn-outline-success sign-in" data-toggle="modal" data-target="#login-modal">Sign In</button>
+						</li>
+
+					</ul>
+
+				</div>
+			</div>
+		</header>;
 }
 
 export default header;
--- src/containers/Tools/Tools.js
+++ src/containers/Tools/Tools.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
 
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
 // import ToolsCSS from './Tools.module.css';
 
 class Tools extends Component {
 	render() {
 		return (
-			<div>
-				<h1>Tools</h1>
+			<div id="Tools">
+				<Tabs justify defaultActiveKey="character">
+					<Tab eventKey="character" title="Character">
+						<h2>Tab Character</h2>
+					</Tab>
+					<Tab eventKey="spells" title="Spells">
+						<h2>Tab Spells</h2>
+					</Tab>
+					<Tab eventKey="roll" title="Roll">
+						<h2>Tab Roll</h2>
+					</Tab>
+					<Tab eventKey="initiative" title="Initiative">
+						<h2>Tab Initiative</h2>
+					</Tab>
+				</Tabs>
 			</div>
 		)
 	};
 }
+
+/*
+<Nav.Item>
+	<Nav.Link eventKey="link-2">Initiative</Nav.Link>
+</Nav.Item>
+<Nav.Item>
+	<Nav.Link eventKey="link-2">Players</Nav.Link>
+</Nav.Item>
+*/
 
 export default Tools;
